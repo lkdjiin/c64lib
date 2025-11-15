ee489859b10ab0790a617e477c0bd7ec39794bc9
Xavier Nayrac <xavier.nayrac@gmail.com>
2025-11-15 08:53:06 +0100

    Add kernal STOP

--- kernal.asm
+++ kernal.asm
@@ -1,19 +1,22 @@
 #if !C64LIB_KERNAL_ASM
 #define C64LIB_KERNAL_ASM
 
-// Print a character at cursor position.
+// Outputs an ASCII character at cursor position.
 //
-// A - The PETSCII character to print.
+// A - The character to print.
 //
 // Destroys nothing.
 .const CHROUT = $ffd2
 
-// Retrieves a single character.
+// Gets an ASCII character
 //
 // Returns the PETSCII character in A. This is non-blocking, if no character
 // then A will contain 0.
 //
 // Destroys A, X.
 .const GETIN = $ffe4
+
+// Checks the RUN/STOP key
+.const STOP = $ffe1
 
 #endif // !C64LIB_KERNAL_ASM
